926c63c1b5092b0af1101c2e68c3af43235db598
Andersonciprian <calenton183@icloud.com>
2023-07-14 17:18:53 -0400

    part 2

--- script.js
+++ script.js
@@ -31,11 +31,23 @@ console.log(topMenuEl);
 
 // task 3.0
 var menuLinks = [
-    {text: 'about', href: '/about'},
-    {text: 'catalog', href: '/catalog'},
-    {text: 'orders', href: '/orders'},
-    {text: 'account', href: '/account'},
-  ];
+  {text: 'about', href: '/about'},
+  {text: 'catalog', href: '#', subLinks: [
+    {text: 'all', href: '/catalog/all'},
+    {text: 'top selling', href: '/catalog/top'},
+    {text: 'search', href: '/catalog/search'},
+  ]},
+  {text: 'orders', href: '#' , subLinks: [
+    {text: 'new', href: '/orders/new'},
+    {text: 'pending', href: '/orders/pending'},
+    {text: 'history', href: '/orders/history'},
+  ]},
+  {text: 'account', href: '#', subLinks: [
+    {text: 'profile', href: '/account/profile'},
+    {text: 'sign out', href: '/account/signout'},
+  ]},
+];
+
 
   // created a variable that assigns elements
   for (let link of menuLinks) {
@@ -44,3 +56,45 @@ var menuLinks = [
     a.innerText = link.text
     topMenuEl.append(a)
   }
+// task 4.0
+  const subMenuEl = document.getElementById('sub-menu')
+
+// task 4.1
+  subMenuEl.style.height = '100%';
+
+// task 4.2
+subMenuEl.style.backgroundColor = 'var(--sub-menu-bg)'
+
+// task 4.3
+subMenuEl.classList.add('flex-around');
+
+
+console.log(subMenuEl.style);
+console.log(subMenuEl.classList);
+
+
+
+console.log(subMenuEl);
+
+subMenuEl.style.position = 'absolute';
+
+// task 4.5
+subMenuEl.style.top = 0
+
+// task 5.1
+const topMenuLinks = document.querySelectorAll('nav#top-menu > a');
+console.log(topMenuLinks);
+
+
+// task 5.6
+
+// const linkFound = menuLinks.find(obj => obj.text === 'account');
+
+// console.log(linkFound);
+
+// if (linkFound.subLinks !== undefined) {
+//   showingSubMenu = true
+// } else {
+//     showingSubMenu = false
+
+// }
